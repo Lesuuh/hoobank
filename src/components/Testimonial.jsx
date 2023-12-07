799a2a34bb52bca15d4914a7a3ebdcabcd2032e2
Lesuuh <lesuuh01@gmail.com>
2023-12-07 11:20:04 +0100

    remove the bg gradient on testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -12,8 +12,8 @@ export const Testimonial = () => {
       </div>
       
         <TestCard/>
-        <div className="absolute z-[3] left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[50%] h-[50%] left-1/2 bottom-0 rounded-full pink__gradient" />
+        {/* <div className="absolute z-[3] left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] left-1/2 bottom-0 rounded-full pink__gradient" /> */}
       
     </section>
   );
